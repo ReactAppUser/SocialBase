72c5361ea418ed9ab0adc642327217dc1e0487db
ReactAppUser <Wec07mer@gmail.com>
2020-11-13 23:37:42 +0200

    Updates

---
+++ precache-manifest.b513649d32ea015b7b04313709794272.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68445d4893d761b5c3a089324c6c8922",
+    "url": "/SocialBase/index.html"
+  },
+  {
+    "revision": "001a1a713ac9957f8ee1",
+    "url": "/SocialBase/static/css/3.661c6260.chunk.css"
+  },
+  {
+    "revision": "947334a6ebdfe44a26cd",
+    "url": "/SocialBase/static/css/4.92acd34b.chunk.css"
+  },
+  {
+    "revision": "512bc48600f12e203fe7",
+    "url": "/SocialBase/static/css/main.1fd1f853.chunk.css"
+  },
+  {
+    "revision": "6d413eef38c7f7e06c41",
+    "url": "/SocialBase/static/js/2.c19e2df8.chunk.js"
+  },
+  {
+    "revision": "001a1a713ac9957f8ee1",
+    "url": "/SocialBase/static/js/3.92b43db4.chunk.js"
+  },
+  {
+    "revision": "947334a6ebdfe44a26cd",
+    "url": "/SocialBase/static/js/4.a04dbed4.chunk.js"
+  },
+  {
+    "revision": "512bc48600f12e203fe7",
+    "url": "/SocialBase/static/js/main.dfb758e5.chunk.js"
+  },
+  {
+    "revision": "0c7571bca71b5befecfb",
+    "url": "/SocialBase/static/js/runtime~main.23074a82.js"
+  },
+  {
+    "revision": "e30aee9794866379c361b1f92221b1b3",
+    "url": "/SocialBase/static/media/Caution.e30aee97.png"
+  },
+  {
+    "revision": "3b7c37417ac0e8b57bad03cf7040ac09",
+    "url": "/SocialBase/static/media/loading.3b7c3741.gif"
+  },
+  {
+    "revision": "bfcb8c238a37ff931819f7fe12a3846b",
+    "url": "/SocialBase/static/media/maxresdefault.bfcb8c23.jpg"
+  },
+  {
+    "revision": "dc4c0ee11eb25de75445269a3356fee3",
+    "url": "/SocialBase/static/media/smile.dc4c0ee1.jpg"
+  }
+]);
